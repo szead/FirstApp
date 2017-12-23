2af405ae2c3e11847f899dcc7e332ebb99259994
szead <adam.szentivanyi@gmail.com>
2017-12-23 19:38:24 +0100

    some changes

---
+++ app/src/main/java/com/szead/firstapp/MyWorker.java
@@ -0,0 +1,12 @@
+package com.szead.firstapp;
+
+/**
+ * Created by Adam on 2017. 12. 23..
+ */
+
+public class MyWorker {
+    public static int doubleTheValue(int value){
+
+        return value * 2;
+    }
+}
